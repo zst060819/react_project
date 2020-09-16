9542bb2d05f979dcf7dd12148c20381d3556e888
xiaotao <769474195@qq.com>
2020-09-16 14:53:59 +0800

    q

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,37 +1,24 @@
 import React, { Component } from 'react'
-// 引入 ECharts 主模块
-import echarts from 'echarts/lib/echarts';
-// 引入柱状图
-import 'echarts/lib/chart/bar';
-// 引入提示框和标题组件
-import 'echarts/lib/component/tooltip';
-import 'echarts/lib/component/title';
+import Card from './components/Card'
+import {Row,Col} from 'antd'
 
-
-class Admin extends Component {
-	componentDidMount() {
-		// 基于准备好的dom，初始化echarts实例
-		var myChart = echarts.init(document.getElementById('main'));
-		// 绘制图表
-		myChart.setOption({
-			title: { text: 'ECharts 入门示例' },
-			tooltip: {},
-			xAxis: {
-				data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-			},
-			yAxis: {},
-			series: [{
-				name: '销量',
-				type: 'bar',
-				data: [5, 20, 36, 10, 10, 20]
-			}]
-		});
-	}
+export default class Admin extends Component {
 	render() {
 		return (
-			<div id="main" style={{ width: 400, height: 400 }}></div>
-		);
+			<Row gutter={10}>
+				<Col xs={24} sm={12} md={12} lg={6} xl={6}>
+					<Card left="123" right="456" content="789" footer="890"/>
+				</Col>
+				<Col xs={24} sm={12} md={12} lg={6} xl={6}>
+					<Card left="123" right="456" content="789" footer="890"/>
+				</Col>
+				<Col xs={24} sm={12} md={12} lg={6} xl={6}>
+					<Card left="123" right="456" content="789" footer="890"/>
+				</Col>
+				<Col xs={24} sm={12} md={12} lg={6} xl={6}>
+					<Card left="123" right="456" content="789" footer="890"/>
+				</Col>
+			</Row>
+		)
 	}
 }
-
-export default Admin;
